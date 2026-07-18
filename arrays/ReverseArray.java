6eb7b63a7d0f3a218c5f24e58ce2711f27377bc9
Swetha S <swetha21032k7@gmail.com>
2026-07-18 22:37:17 +0530

    Day 5 - Solved Reverse Array using Two Pointer Technique

---
+++ arrays/ReverseArray.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void reverseArray(int[] arr) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        reverseArray(arr);
+
+        System.out.println("Reversed Array:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
